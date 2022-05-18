f09d10f8c62a000bea57f9d6cbf16b5327d3ee7b
Alex Conder <alex@kenzie.com.br>
2022-05-18 13:43:50 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FirstExample from './components/FirstExample';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirstExample/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+//Component
+const TextExample = styled.h1`
+    // CSS
+    font-family: serif;
+    font-size: 2rem;
+    //Javascript
+    color: ${(props) => props.color};
+`
+
+const FirstExample = () => {
+  return (
+    <div>
+        { /* Passagem de props e importação de componente */ }
+        <TextExample color="red">Teste</TextExample>
+    </div>
+  )
+}
+
+export default FirstExample
